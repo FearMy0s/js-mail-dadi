3f512920f79b910bce649e2263e2dd09318b4bb3
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-03 16:21:47 +0200

    problema

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 //creo il "database" in cui sono contenuti i dati
 const users = ["antonio","luca","andrea"];
 //faccio immetere l'user name da inserire
+const Access = 0;
 const  Insert = (prompt("Inserisci Nome Utente"));
 for(let i = 0; i<users.length;i++){
 if(users[i] === Insert){
+    Check = Access + 1;
+}
+}
+if(Check != 1){
     document.getElementById("visual").innerHTML = `Accesso Effettuato`;
 }
 else{
     document.getElementById("visual").innerHTML = `Accesso Negato`;
 }
-}
+console.log(Check)
