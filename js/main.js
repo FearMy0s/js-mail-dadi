3de5ec9632b5929c564a09f18b392a8756282cc4
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-03 15:26:41 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //creo il "database" in cui sono contenuti i dati
-const users = ["antonio","luca@hotmail.it","andrea@hotmail.it"]
+const users = ["antonio","luca@hotmail.it","andrea@hotmail.it"];
 //faccio immetere l'user name da inserire
 const  Insert = (prompt("Inserisci Nome Utente"));
-if(users == Insert[i]){
-    console.log("Accesso Effettuato")
+for(let i = 0; i<users.length;i++){
+if(users[i] === Insert){
+    
 }else{
     console.log("Username Non Registrato")
+}
 }
