51b6b28cba058ebb17cd03ef30024478c3fbeb26
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-03 15:12:46 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//creo il "database" in cui sono contenuti i dati
+const users = ["antonio","luca@hotmail.it","andrea@hotmail.it"]
+//faccio immetere l'user name da inserire
+const  Insert = (prompt("Inserisci Nome Utente"));
+if(users == Insert[i]){
+    console.log("Accesso Effettuato")
+}else{
+    console.log("Username Non Registrato")
+}
