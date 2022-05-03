5d23215b7cdc6b1c626ed249677e225b463e2eb0
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-03 16:44:25 +0200

    finito primo esercizio

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 //creo il "database" in cui sono contenuti i dati
-const users = ["antonio","luca","andrea"];
+const users = ["antonio886","luca942","andrea765"];
 //faccio immetere l'user name da inserire
-const Access = 0;
+let Access = false;
 const  Insert = (prompt("Inserisci Nome Utente"));
 for(let i = 0; i<users.length;i++){
 if(users[i] === Insert){
-    Check = Access + 1;
+     Access = true;
 }
 }
-if(Check === 1){
+if(Access === true){
     document.getElementById("visual").innerHTML = `Accesso Effettuato`;
 }
 else{
     document.getElementById("visual").innerHTML = `Accesso Negato`;
 }
-console.log(Check)
